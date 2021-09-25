31e5c510333ea1f5743f7fc3ad756bd940c0b564
alexandex <kienva1992@gmail.com>
2021-09-25 16:34:11 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CollectionsStringsFiless
+{
+    class Program
+    {
+        private static string DecodeMessage(string[] lines)
+        {
+			string decodedMessage = "";
+			for (int i = lines.Length - 1; i >= 0; i--)
+			{
+				string word = lines[i];
+				string[] words = word.Split(' ');
+
+				for (int k = words.Length - 1; k >= 0; k--)
+				{
+					string res = words[k];
+					if ((res.Length != 0) && char.IsUpper(res[0]))
+					{
+						decodedMessage = decodedMessage + res + " ";
+					}
+				}
+			}
+			return decodedMessage;
+		}
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
