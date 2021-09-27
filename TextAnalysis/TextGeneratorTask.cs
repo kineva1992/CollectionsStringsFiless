58ab7c78d0ad82cab43371d0bd758430a5df7534
alexandex <kienva1992@gmail.com>
2021-09-27 21:25:31 +0300

    123

--- TextAnalysis/TextGeneratorTask.cs
+++ TextAnalysis/TextGeneratorTask.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TextAnalysis
 {
@@ -9,7 +10,32 @@ namespace TextAnalysis
             string phraseBeginning,
             int wordsCount)
         {
+            string result = string.Empty;
+          
+            for (int i = 0; i < wordsCount; ++i)
+            {
+                string[] splitText = phraseBeginning.Split(' ');
+                if (splitText.Length == 0)
+                    break;
+                if (splitText.Length >= 2)
+                {
+                    if (nextWords.ContainsKey(string.Format("{0} {1}", splitText[splitText.Length - 2], 
+                        splitText[splitText.Length - 1])))
+                    {
+                        phraseBeginning += " " + nextWords[string.Format("{0} {1}", 
+                            splitText[splitText.Length - 2], splitText[splitText.Length - 1])];
+                        continue;
+                    }
+                }
+                if (nextWords.ContainsKey(splitText[splitText.Length - 1]))
+                {
+                    phraseBeginning += " " + nextWords[splitText[splitText.Length - 1]];
+                    continue;
+                }
+                break;
+            }
             return phraseBeginning;
         }
     }
+
 }
